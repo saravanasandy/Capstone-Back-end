2bb14f8b333c7611ec5da68faa3ef3f62dee1d4b
saravanasandy <msaravanakumar369@gmail.com>
2024-05-06 17:36:42 +0530

    new change

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes'))
+app.use('/api/flights',require('./routes/flightRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
 
 app.use(errorHandler)
