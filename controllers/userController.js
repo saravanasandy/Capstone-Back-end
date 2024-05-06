37c09f8ec30716f3ad1205694820c3acb5f7a671
saravanasandy <msaravanakumar369@gmail.com>
2024-05-06 19:08:17 +0530

    new change

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,12 +86,14 @@ const  loginUser = asyncHandler(async (req,res)=> {
 
 const  getMe = asyncHandler(async (req,res)=> {
 
-  const {_id, name, email} = await User.findById(req.user.id)
-   res.status(200).json({
-    id: _id,
-    name,
-    email,
-   })
+//   const {_id, name, email} = await User.findById(req.user.id)
+//    res.status(200).json({
+//     id: _id,
+//     name,
+//     email,
+//    })
+
+   res.status(200).json(req.user)
 
     // res.json({message: 'User data display'})
 })
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require("cors");
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
@@ -9,9 +10,13 @@ connectDB()
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    orgin: "http://localhost:3000"
+}));
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals',require('./routes/goalRoutes'))
+
+app.use('/api/flights',require('./routes/flightRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
 
 app.use(errorHandler)
